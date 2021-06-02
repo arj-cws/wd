d2a3a407492f4ea9c2c861e7b6e8e70c73f696ff
Son Chang Wook <songoon79@gmail.com>
2021-06-03 02:10:19 +0900

    page 정리

--- resource/load-resource-foot.js
+++ resource/load-resource-foot.js
@@ -1,4 +1,4 @@
 //// 문서 출력 이후 로드할 리소스 - 순서 유지, <body> 끝에 삽입 //// 
 
 // js - control-common-ui-foot
-document.write("<script type='text/javascript' src='../resource/js/control-common-ui.js'></script>");
+document.write("<script type='text/javascript' src='./resource/js/control-common-ui.js'></script>");
